d8050908372db1619778e6d7fa1ae2194aa48637
Ca.Men <camenlx@users.noreply.github.com>
2018-12-10 00:01:23 +0000

    0.12.3.4 2019 (c) 01coin
    to remove warning reuse 0.12.3.4 for mini project again
    release false as this is test sw

--- src/clientversion.h
+++ src/clientversion.h
@@ -18,10 +18,10 @@
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 12
 #define CLIENT_VERSION_REVISION 3
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 4
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
+#define CLIENT_VERSION_IS_RELEASE false
 
 /**
  * Copyright year (2009-this)
@@ -39,7 +39,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " The ZeroOne Core Developers"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " The 01coin Developers"
 
 /**
  * zerooned-res.rc includes this file, but it cannot cope with real c++ code.
