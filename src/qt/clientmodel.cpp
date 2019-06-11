f76aa382d099bf5ccdf35066bfc21b6011208f7f
root <camenlx@users.noreply.github.com>
2019-06-11 03:05:43 +0000

    qt clientmodel.cpp:87 tr string

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -82,8 +82,8 @@ int ClientModel::getNumConnections(unsigned int flags) const
 
 QString ClientModel::getMasternodeCountString() const
 {
-    // return tr("Total: %1 (PS compatible: %2 / Enabled: %3) (IPv4: %4, IPv6: %5, TOR: %6)").arg(QString::number((int)mnodeman.size()))
-    return tr("Total: %1 (PS compatible: %2 / Enabled: %3)")
+    return tr("Total: %1 (PS compatible: %2 / Enabled: %3) (IPv4: %4, IPv6: %5, TOR: %6)")
+    //return tr("Total: %1 (PS compatible: %2 / Enabled: %3)")
             .arg(QString::number((int)mnodeman.size()))
             .arg(QString::number((int)mnodeman.CountEnabled(MIN_PRIVATESEND_PEER_PROTO_VERSION)))
             .arg(QString::number((int)mnodeman.CountEnabled()))
