aebf2b974d9305f964afee61251cad1b5e5d0762
camenlx <camenlx@users.noreply.github.com>
2019-09-04 20:28:38 +0100

    BIP44/SLIP44 CoinType 399 for ZOC

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -168,8 +168,9 @@ public:
         // ZeroOne BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // ZeroOne BIP44 coin type was '5'
-        nExtCoinType = 5;
+        // ZeroOne BIP44 coin type was '5', requested '399' for ZOC at SLIP44
+        // see https://github.com/satoshilabs/slips/pull/755
+        nExtCoinType = 399;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
@@ -300,7 +301,7 @@ public:
         // Testnet ZeroOne BIP32 prvkeys start with 'tprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
 
-        // ZeroOne BIP44 coin type is '1'
+        // ZeroOne BIP44 coin type is '1' for tZOC
         nExtCoinType = 1;
 
         fMiningRequiresPeers = true;
@@ -423,7 +424,7 @@ public:
         // Regtest ZeroOne BIP32 prvkeys start with 'tprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
 
-        // ZeroOne BIP44 coin type is '1'
+        // ZeroOne BIP44 coin type is '1' for tZOC
         nExtCoinType = 1;
    }
 
