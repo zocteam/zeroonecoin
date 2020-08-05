a454735404afa74fd170c4d031a87312da716f8a
camenlx <camenlx@users.noreply.github.com>
2020-08-05 18:33:36 +0100

    ci travis 2nd-try

--- ci/test_integrationtests.sh
+++ ci/test_integrationtests.sh
@@ -8,7 +8,7 @@ PASS_ARGS="$@"
 
 source ./ci/matrix.sh
 
-if [ "$RUN_TESTS" != "true" ]; then
+if [ "$RUN_INTEGRATIONTESTS" != "true" ]; then
   echo "Skipping integration tests"
   exit 0
 fi
@@ -18,3 +18,12 @@ export LD_LIBRARY_PATH=$BUILD_DIR/depends/$HOST/lib
 cd build-ci/zeroonecore-$BUILD_TARGET
 
 ./qa/pull-tester/rpc-tests.py --coverage $PASS_ARGS
+
+set +e
+./test/functional/test_runner.py --coverage --quiet --nocleanup --tmpdir=$(pwd)/testdatadirs $PASS_ARGS
+RESULT=$?
+set -e
+
+echo "Collecting logs..."
+BASEDIR=$(ls testdatadirs)
+exit $RESULT
